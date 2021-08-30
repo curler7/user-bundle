7a6990b5f5794301e8a593bf81e822f225ef2d28
Gunnar <suwe@smart-media.design>
2021-08-30 18:41:11 +0200

    s

--- src/Bridge/ApiPlatform/UserNormalizer.php
+++ src/Bridge/ApiPlatform/UserNormalizer.php
@@ -26,12 +26,13 @@ use Symfony\Component\Serializer\SerializerInterface;
 /**
  * @author Gunnar Suwe <suwe@smart-media.design>
  */
-class UserNormalizer implements ContextAwareDenormalizerInterface, SerializerAwareInterface
+class UserNormalizer implements ContextAwareDenormalizerInterface, DenormalizerAwareInterface
 {
     use DenormalizerAwareTrait;
 
+    private const ALREADY_CALLED = 'USER_NORMALIZER';
+
     public function __construct(
-        private DenormalizerInterface $decorated,
         private CanonicalFieldsUpdaterInterface $canonicalFieldsUpdater,
         private PasswordUpdaterInterface $passwordUpdater,
         private string $resourceClass
@@ -39,38 +40,30 @@ class UserNormalizer implements ContextAwareDenormalizerInterface, SerializerAwa
 
     public function supportsDenormalization($data, $type, $format = null, array $context = []): bool
     {
-        return $this->decorated->supportsDenormalization($data, $type, $format);
+        return $this->resourceClass === $type && !($context[self::ALREADY_CALLED] ?? null);
     }
 
-    public function denormalize($data, $type, $format = null, array $context = []): mixed
+    public function denormalize($data, $type, $format = null, array $context = []): UserInterface
     {
+        $context[self::ALREADY_CALLED] = true;
+
         /** @var UserInterface $user */
         $user = $this->denormalizer->denormalize($data, $type, $format, $context);
 
-        if ($user instanceof UserInterface) {
+        // $user->setUsernameCanonical($this->canonicalFieldsUpdater->canonicalizeUsername($data['username']));
+        // $user->setEmailCanonical($this->canonicalFieldsUpdater->canonicalizeEmail($data['email']));
+        // $this->passwordUpdater->hashPassword($user);
 
+        if ($data['username'] ?? null && $data['username'] !== $context['previous_data']) {
             $user->setUsernameCanonical($this->canonicalFieldsUpdater->canonicalizeUsername($data['username']));
+        }
+        if ($data['email'] ?? null && $data['email'] !== $context['previous_data']) {
             $user->setEmailCanonical($this->canonicalFieldsUpdater->canonicalizeEmail($data['email']));
+        }
+        if ($data['plainPassword'] ?? null) {
             $this->passwordUpdater->hashPassword($user);
-
-            if ($data['username'] ?? null && $data['username'] !== $context['previous_data']) {
-                $user->setUsernameCanonical($this->canonicalFieldsUpdater->canonicalizeUsername($data['username']));
-            }
-            if ($data['email'] ?? null && $data['email'] !== $context['previous_data']) {
-                $user->setEmailCanonical($this->canonicalFieldsUpdater->canonicalizeEmail($data['email']));
-            }
-            if ($data['plainPassword'] ?? null) {
-                $this->passwordUpdater->hashPassword($user);
-            }
         }
 
         return $user;
-    }
-
-    public function setSerializer(SerializerInterface $serializer)
-    {
-        if($this->decorated instanceof SerializerAwareInterface) {
-            $this->decorated->setSerializer($serializer);
-        }
     }
 }
