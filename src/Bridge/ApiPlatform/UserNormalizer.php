e4054d34ecf1cf549cae2b53c43faf1c13408fc4
Gunnar <suwe@smart-media.design>
2021-08-30 19:33:22 +0200

    s

--- src/Bridge/ApiPlatform/UserNormalizer.php
+++ src/Bridge/ApiPlatform/UserNormalizer.php
@@ -16,51 +16,62 @@ namespace Curler7\UserBundle\Bridge\ApiPlatform;
 use Curler7\UserBundle\Model\UserInterface;
 use Curler7\UserBundle\Util\CanonicalFieldsUpdaterInterface;
 use Curler7\UserBundle\Util\PasswordUpdaterInterface;
-use Symfony\Component\Serializer\Normalizer\ContextAwareDenormalizerInterface;
-use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
-use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerAwareInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @author Gunnar Suwe <suwe@smart-media.design>
  */
-class UserNormalizer implements ContextAwareDenormalizerInterface, DenormalizerAwareInterface
+final class UserNormalizer implements NormalizerInterface, DenormalizerInterface, SerializerAwareInterface
 {
-    use DenormalizerAwareTrait;
-
-    private const ALREADY_CALLED = 'USER_NORMALIZER';
-
     public function __construct(
+        private DenormalizerInterface $decorated,
         private CanonicalFieldsUpdaterInterface $canonicalFieldsUpdater,
         private PasswordUpdaterInterface $passwordUpdater,
         private string $resourceClass
     ) {}
 
-    public function supportsDenormalization($data, $type, $format = null, array $context = []): bool
+    public function supportsNormalization($data, $format = null): bool
     {
-        return $this->resourceClass === $type && !($context[self::ALREADY_CALLED] ?? null);
+        return $this->decorated->supportsNormalization($data, $format);
+    }
+
+    public function normalize($object, $format = null, array $context = []): float|int|bool|\ArrayObject|array|string|null
+    {
+        return $this->decorated->normalize($object, $format, $context);
+    }
+
+    public function supportsDenormalization($data, $type, $format = null): bool
+    {
+        return $this->decorated->supportsDenormalization($data, $type, $format);
     }
 
     public function denormalize($data, $type, $format = null, array $context = []): UserInterface
     {
-        $context[self::ALREADY_CALLED] = true;
-
         /** @var UserInterface $user */
-        $user = $this->denormalizer->denormalize($data, $type, $format, $context);
+        $user = $this->decorated->denormalize($data, $type, $format, $context);
 
-        // $user->setUsernameCanonical($this->canonicalFieldsUpdater->canonicalizeUsername($data['username']));
-        // $user->setEmailCanonical($this->canonicalFieldsUpdater->canonicalizeEmail($data['email']));
-        // $this->passwordUpdater->hashPassword($user);
-
-        if ($data['username'] ?? null && $data['username'] !== $context['previous_data']) {
-            $user->setUsernameCanonical($this->canonicalFieldsUpdater->canonicalizeUsername($data['username']));
-        }
-        if ($data['email'] ?? null && $data['email'] !== $context['previous_data']) {
-            $user->setEmailCanonical($this->canonicalFieldsUpdater->canonicalizeEmail($data['email']));
-        }
-        if ($data['plainPassword'] ?? null) {
-            $this->passwordUpdater->hashPassword($user);
+        if ($this->resourceClass === $type) {
+            if ($data['username'] ?? null && $data['username'] !== $context['previous_data']) {
+                $user->setUsernameCanonical($this->canonicalFieldsUpdater->canonicalizeUsername($data['username']));
+            }
+            if ($data['email'] ?? null && $data['email'] !== $context['previous_data']) {
+                $user->setEmailCanonical($this->canonicalFieldsUpdater->canonicalizeEmail($data['email']));
+            }
+            if ($data['plainPassword'] ?? null) {
+                $this->passwordUpdater->hashPassword($user);
+            }
         }
 
         return $user;
+    }
+
+    public function setSerializer(SerializerInterface $serializer)
+    {
+        if($this->decorated instanceof SerializerAwareInterface) {
+            $this->decorated->setSerializer($serializer);
+        }
     }
 }
