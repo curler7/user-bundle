0ef0438f940a574718034bb54f97b83cb9e62d53
Gunnar <suwe@smart-media.design>
2021-09-07 13:29:14 +0200

    s

--- tests/DependencyInjection/Curler7UserExtensionTest.php
+++ tests/DependencyInjection/Curler7UserExtensionTest.php
@@ -24,8 +24,10 @@ use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
 class Curler7UserExtensionTest extends AbstractExtensionTestCase
 {
     private array $default = [
-        'user_class' => User::class,
-        'group_class' => Group::class,
+        'model' => [
+            'user_class' => User::class,
+            'group_class' => Group::class,
+        ]
     ];
 
     protected function getContainerExtensions(): array
@@ -46,30 +48,33 @@ class Curler7UserExtensionTest extends AbstractExtensionTestCase
     {
         $this->load($this->default);
 
+        $this->assertContainerBuilderHasService('curler7_user.api_platform.auto_group_resource_metadata_factory');
+        $this->assertContainerBuilderHasService('curler7_user.command.create_user');
+        $this->assertContainerBuilderHasService('curler7_user.controller.login_link');
+        $this->assertContainerBuilderHasService('curler7_user.model.group_manager');
+        $this->assertContainerBuilderHasService('curler7_user.model.user_manager');
+        $this->assertContainerBuilderHasService('curler7_user.model.object_manager');
+        $this->assertContainerBuilderHasService('curler7_user.open_api.jwt_decorator');
+        $this->assertContainerBuilderHasService('curler7_user.security.groups_context_builder');
+        $this->assertContainerBuilderHasService('curler7_user.security.authentication_success_handler');
+        $this->assertContainerBuilderHasService('curler7_user.security.user_voter');
+        $this->assertContainerBuilderHasService('curler7_user.security.group_voter');
+        $this->assertContainerBuilderHasService('curler7_user.serializer.user_normalizer');
+        $this->assertContainerBuilderHasService('curler7_user.subscriber.jwt_subscriber');
+        $this->assertContainerBuilderHasService('curler7_user.subscriber.validate_before_delete');
+        $this->assertContainerBuilderHasService('curler7_user.util.canonical_fields_updater');
+        $this->assertContainerBuilderHasService('curler7_user.util.canonicalizer');
+        $this->assertContainerBuilderHasService('curler7_user.util.password_updater');
+        $this->assertContainerBuilderHasService('curler7_user.util.user_registration');
+        $this->assertContainerBuilderHasService('curler7_user.util.user_spy');
         $this->assertContainerBuilderHasService('curler7_user.util.email_canonicalizer');
         $this->assertContainerBuilderHasService('curler7_user.util.username_canonicalizer');
-        $this->assertContainerBuilderHasService('curler7_user.util.password_updater');
-        $this->assertContainerBuilderHasService('curler7_user.user_manager');
-
-        $this->assertContainerBuilderHasParameter('curler7_user.api_platform');
+        $this->assertContainerBuilderHasService('curler7_user.validator.last_super_admin_user');
 
         $container = $this->container;
-        $this->assertTrue($container->getParameter('curler7_user.api_platform'));
-    }
-
-    public function testApiPlatformLoading()
-    {
-        $config = array_merge($this->default, ['api_platform' => true]);
-        $this->load($config);
-
-        $this->assertContainerBuilderHasService('curler7_user.user_denormalizer');
-    }
-
-    public function testGroupLoading()
-    {
-        $this->load($this->default);
-
-        $this->assertContainerBuilderHasService('curler7_user.group_manager');
-        $this->assertContainerBuilderHasParameter('curler7_user.model.group.class');
+        $this->assertIsString($container->getParameter('curler7_user.model.user.class'));
+        $this->assertIsString($container->getParameter('curler7_user.model.group.class'));
+        $this->assertIsString($container->getParameter('curler7_user.model.model_manager_name'));
+        $this->assertIsString($container->getParameter('curler7_user.model.storage'));
     }
 }
