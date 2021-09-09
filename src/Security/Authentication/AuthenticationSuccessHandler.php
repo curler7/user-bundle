8a976e4e9244ea361b439e6921ebc9bfddeef553
Gunnar <suwe@smart-media.design>
2021-09-09 23:36:52 +0200

    s

--- src/Security/Authentication/AuthenticationSuccessHandler.php
+++ src/Security/Authentication/AuthenticationSuccessHandler.php
@@ -14,6 +14,8 @@ declare(strict_types=1);
 namespace Curler7\UserBundle\Security\Authentication;
 
 use Curler7\UserBundle\Model\UserInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
@@ -24,13 +26,21 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerI
  */
 class AuthenticationSuccessHandler implements AuthenticationSuccessHandlerInterface
 {
+    public function __construct(protected JWTTokenManagerInterface $JWTTokenManager)
+    {}
+
     public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
     {
         /** @var UserInterface $user */
         $user = $token->getUser();
-
         $user->setEnabled(true);
+        $this->JWTTokenManager->create($user);
 
-        return new RedirectResponse('/admin');
+        return $this->generateResponse($request, $token);
+    }
+
+    protected function generateResponse(Request $request, TokenInterface $token): RedirectResponse|JsonResponse
+    {
+        return new RedirectResponse('/');
     }
 }
